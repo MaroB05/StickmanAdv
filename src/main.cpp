79943f00688c74d5ab21a56f1936a4aff3e6c7f1
marwan basem <marob27105@gmail.com>
2026-02-21 21:30:57 +0200

    moved terminal configuration to config.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,6 @@
-#include <iostream>
-using namespace std;
-#include "screen.h"
-
-#if defined(__linux__)
-#include <unistd.h>
-#include <termios.h>
-#elif defined(_WIN32)
-#include <windows.h>
-#endif
-
-void terminal_config(){
-  #if defined(__linux__)
-    static struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO); // Disable canonical mode and echo
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  #elif defined(_WIN32)
-    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
-    DWORD mode;
-    GetConsoleMode(hInput, &mode);
-
-    // Equivalent to disabling ICANON and ECHO
-    mode &= ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT);
-    SetConsoleMode(hInput, mode);
-  #endif
-}
+#include "controller.h"
 
 int main() {
-  terminal_config();
 
   scene window = scene(40, 40);
   

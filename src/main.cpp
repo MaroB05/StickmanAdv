beec6f49c2361b78b4f0c1754b76f212e14fccd0
marwan basem <marob27105@gmail.com>
2026-02-21 21:31:26 +0200

    updated main using Controller class

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,8 @@
 
 int main() {
 
-  scene window = scene(40, 40);
-  
-  char key_pressed = ' ';
-  window.add_stickman(10, 10);
-  stickman* player = window.get_object(0);
+  Controller game = Controller();
+  game.run(40, 40);
 
-  while (key_pressed != 'q'){
-    window.init();
-    window.draw_objects();
-    window.render();
-    key_pressed = getchar();
-    if (key_pressed == 'w')
-      player->move(0,-1);
-    else if (key_pressed == 's')
-      player->move(0,1);
-    else if (key_pressed == 'a')
-      player->move(-1,0);
-    else if (key_pressed == 'd')
-      player->move(1,0);
-  }
-  cout << "Game Ended!\n";
   return 0;
 }

045ce073677efac589a103d91aa72492bd45eaab
marwan basem <marob27105@gmail.com>
2026-02-22 05:03:40 +0200

    initial implementation of the Objects_array class

--- src/object.h
+++ src/object.h
@@ -21,6 +21,10 @@ public:
     sprite_size = width*height;
   }
 
+  ~Object(){
+    delete[] sprites;
+  }
+
   int get_width(){
     return width;
   }
@@ -77,7 +81,94 @@ public:
 
 class Objects_array{
 private:
-  
+  Object** array = nullptr;
+  Object** temp = nullptr; // instead of allocating and deallocating temp pointer
+  int capacity, elements;
 public:
+  Objects_array(){
+    capacity = 0;
+    elements = 0;
+  }
+
+  Objects_array(int n){
+    elements = 0;
+    capacity = n;
+  }
+
+  Object* get_element(int index){
+    if (index < elements)
+      return array[index];
+    return nullptr;
+  }
+
+  void* set_element(int index, Object* obj){
+    array[index] = obj;
+  }
+
+  int find(Object* obj){
+    for (int i = 0; i < elements; i++){
+      if (array[i] == obj)
+        return i;
+    }
+    return -1;
+  }
+
+  // move from one array to a new one
+  void move_elements(){
+    for (int i = 0; i < elements; i++)
+      array[i] = temp[i];
+  }
+
+  inline int is_full(){
+    return (capacity == elements);
+  }
+
+  void grow(){
+    if (capacity == 0){
+      capacity = 1;
+      array = new Object*[capacity];
+      return;
+    }
+    capacity *= 2;
+    temp = array;
+    array = new Object*[capacity];
+    move_elements();
+    delete[] temp;
+    temp = nullptr;
+  }
+
+  void shrink(){
+    if (capacity <= 1)
+      return;
+
+    capacity /= 2;
+    temp = array;
+    array = new Object*[capacity];
+    move();
+    delete[] temp;
+    temp = nullptr;
+  }
+
+  void add_object(Object* obj){
+    if (is_full())
+      grow();
+    array[elements] = obj;
+    elements++;
+  }
+
+  void remove_object(int index){
+    delete array[index];
+    array[index] = nullptr;
+    elements--;
+  }
+
+  void remove_object(Object* obj){
+    int index = find(obj);
+    if (index == -1)
+      return;
+    delete array[index];
+    array[index] = array[elements-1];
+    elements--;
+  }
 
 };

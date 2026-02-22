3d3811a882a6364282dbb47ed06b4e5d15ef3bd1
marwan basem <marob27105@gmail.com>
2026-02-22 03:11:16 +0200

    reimplement Stickman as inherited from Object class

--- src/stickman.h
+++ src/stickman.h
@@ -1,72 +1,23 @@
 #include "point.h"
-class Stickman{
-private:
-  Point pos;
-  // int x,y;
-  int state;
-  static const int state_num = 3;
-  static const int width = 3;
-  static const int height = 4;
-  static const int sprite_size = width * height;
-  char sprites[state_num][sprite_size] = {{' ', 'o', ' ', '/', '|', '\\', ' ', '|', ' ', '/', ' ', '\\'}
-  , {' ', 'o', ' ', '\\', '|', '/', ' ', '|', ' ', '/', '|', ' '}
-  , {' ', 'o', ' ', '\\', '|', '/', ' ', '|', ' ', ' ', '|', '\\'}};
+#include "object.h"
 
+#define WIDTH 3
+#define HEIGHT 4
+#define STATE_NUM 3
+
+class Stickman : public Object{
 public:
-  Stickman(int a, int b){
+  Stickman(int a, int b): Object(WIDTH, HEIGHT, STATE_NUM){
     pos = Point(a,b);
     state = 0;
-  }
-
-  void print_sprite(int c){
-    if (c < state_num && c >= 0)
-      for (int i = 0; i < width * height; i++)
-        cout << sprites[c][i];
-  }
-
-  int get_width(){
-    return width;
-  }
-
-  int get_height(){
-    return height;
-  }
-
-  Point get_pos(){
-    return pos;
-  }
-
-  void set_pos(int a, int b){
-    pos.x = a;
-    pos.y = b;
-  }
-
-  void set_y(int b){
-    pos.y = b;
-  }
-
-  void set_x(int a){
-    pos.x = a;
-  }
-
-  int px(){
-    return pos.x;
-  }
-
-  int py(){
-    return pos.y;
-  }
-
-  char* get_sprite(){
-    return sprites[state];
-  }
-
-  void next_state(){
-    state = (state+1) % state_num;
-  }
-
-  void move(int a, int b){
-    pos.x += a;
-    pos.y += b;
+    char sprites_temp[state_num][sprite_size] = {{' ', 'o', ' ', '/', '|', '\\', ' ', '|', ' ', '/', ' ', '\\'}
+    , {' ', 'o', ' ', '\\', '|', '/', ' ', '|', ' ', '/', '|', ' '}
+    , {' ', 'o', ' ', '\\', '|', '/', ' ', '|', ' ', ' ', '|', '\\'}};
+    sprites = new char*[state_num];
+    for (int i = 0; i < state_num; i++){
+      sprites[i] = new char[sprite_size];
+      for (int j = 0; j < sprite_size; j++)
+        sprites[i][j] = sprites_temp[i][j];
+    }
   }
 };

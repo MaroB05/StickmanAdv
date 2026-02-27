26947b3b60c69e372a76c4eed86c017da9f291a3
marwan basem <marob27105@gmail.com>
2026-02-27 03:10:58 +0200

    render the ground in the scene

--- src/engine/screen.h
+++ src/engine/screen.h
@@ -7,13 +7,16 @@ public:
   Scene(int w, int h){
     width = w;
     height = h;
-    pixels = new char*[height];
+    pixels = new char*[height+1];
     for (int i = 0; i < height; i++){
       pixels[i] = new char[width+2];
       fill(pixels[i], pixels[i]+width, ' ');
       pixels[i][width] = '|';
       pixels[i][width+1] = '\0';
     }
+    pixels[height] = new char[width+2];
+    fill(pixels[height], pixels[height]+width, '-');
+    pixels[height][width+1] = '\0';
   }
 
   int get_width() const{
@@ -44,7 +47,7 @@ public:
   }
 
   void render(){
-    for (int i = 0; i < height; i++)
+    for (int i = 0; i < height+1; i++)
         cout << pixels[i] << endl;
   }
 
